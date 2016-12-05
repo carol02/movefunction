2f35cc4107b22c32810eb246991e433afd6ea24e
carol02 <1057889038@qq.com>
2016-12-05 09:23:12 +0800

    .

---
+++ 1.js
@@ -0,0 +1,13 @@
+function $( v ){
+	if( typeof v === 'function' ){
+		window.onload = v;
+	} else if ( typeof v === 'string' ) {
+		return document.getElementById(v);
+	} else if ( typeof v === 'object' ) {
+		return v;
+	}
+}
+
+function getStyle( obj, attr ){
+	return obj.currentStyle ? obj.currentStyle[attr] : getComputedStyle( obj )[attr];
+}
